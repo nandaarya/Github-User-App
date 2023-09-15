d014e29fe82d1bf4776c6601e69da7af298c8097
Nanda Arya Putra <nandaarya404@gmail.com>
2023-09-15 17:20:49 +0700

    get userData from username and add detail page

---
+++ app/src/main/java/com/example/github_user_app/ui/DetailUserActivity.kt
@@ -0,0 +1,41 @@
+package com.example.github_user_app.ui
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.github_user_app.databinding.ActivityDetailUserBinding
+
+class DetailUserActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailUserBinding
+    private val githubUserDetailViewModel by viewModels<GithubUserDetailViewModel>()
+    private lateinit var username: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUserData()
+    }
+
+    private fun setUserData() {
+        username = intent.getStringExtra("username")!!
+
+        githubUserDetailViewModel.getGithubUserDetail(username)
+
+        githubUserDetailViewModel.githubUserDetail.observe(this) { userDetail ->
+            Glide
+                .with(this)
+                .load(userDetail.avatarUrl)
+                .fitCenter()
+                .into(binding.ivProfile)
+
+            binding.tvUsername.text = userDetail.login
+            binding.tvName.text = userDetail.name
+            binding.tvTotalFollowers.text = userDetail.followers.toString()
+            binding.tvTotalFollowing.text = userDetail.following.toString()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/github_user_app/ui/GithubUserDetailViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.github_user_app.ui
+
+import android.content.ContentValues
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.github_user_app.data.response.GithubUserDetailResponse
+import com.example.github_user_app.data.retrofit.ApiConfig
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class GithubUserDetailViewModel: ViewModel() {
+
+    private val _githubUserDetail = MutableLiveData<GithubUserDetailResponse>()
+    val githubUserDetail: LiveData<GithubUserDetailResponse> = _githubUserDetail
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    fun getGithubUserDetail(username: String) {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().getGithubUserDetail(username)
+        client.enqueue(object : Callback<GithubUserDetailResponse> {
+            override fun onResponse(
+                call: Call<GithubUserDetailResponse>,
+                response: Response<GithubUserDetailResponse>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _githubUserDetail.value = response.body()
+                } else {
+                    Log.e(ContentValues.TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<GithubUserDetailResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(ContentValues.TAG, "onFailure: ${t.message.toString()}")
+            }
+        })
+    }
+}
