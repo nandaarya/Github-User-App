7d33e5695c0bedfe2f37573be870536dd100897f
Nanda Arya Putra <nandaarya404@gmail.com>
2023-09-15 17:52:58 +0700

    fix GITHUB TOKEN leaks

--- app/src/main/java/com/example/github_user_app/data/retrofit/ApiService.kt
+++ app/src/main/java/com/example/github_user_app/data/retrofit/ApiService.kt
@@ -1,5 +1,6 @@
 package com.example.github_user_app.data.retrofit
 
+import com.example.github_user_app.BuildConfig
 import com.example.github_user_app.data.response.GithubUserDetailResponse
 import com.example.github_user_app.data.response.GithubUserResponse
 import retrofit2.Call
@@ -9,14 +10,15 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
-    @Headers("Authorization: token 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")
+
     @GET("search/users")
+    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
     fun getGithubUser(
         @Query("q") username: String
     ): Call<GithubUserResponse>
 
-    @Headers("Authorization: token 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")
     @GET("users/{username}")
+    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
     fun getGithubUserDetail(
         @Path("username") username: String
     ): Call<GithubUserDetailResponse>
