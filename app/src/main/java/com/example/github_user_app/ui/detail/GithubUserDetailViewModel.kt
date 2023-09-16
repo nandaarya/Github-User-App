d11aef9b850ab67e2b0003a3fc18fac8e19c05f9
Nanda Arya Putra <nandaarya404@gmail.com>
2023-09-16 18:15:18 +0700

    add coroutines

--- app/src/main/java/com/example/github_user_app/ui/detail/GithubUserDetailViewModel.kt
+++ app/src/main/java/com/example/github_user_app/ui/detail/GithubUserDetailViewModel.kt
@@ -5,8 +5,11 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.github_user_app.data.response.DetailUserResponse
 import com.example.github_user_app.data.retrofit.ApiConfig
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,24 +24,26 @@ class GithubUserDetailViewModel: ViewModel() {
 
     fun getGithubUserDetail(username: String) {
         _isLoading.value = true
-        val client = ApiConfig.getApiService().getGithubUserDetail(username)
-        client.enqueue(object : Callback<DetailUserResponse> {
-            override fun onResponse(
-                call: Call<DetailUserResponse>,
-                response: Response<DetailUserResponse>
-            ) {
-                _isLoading.value = false
-                if (response.isSuccessful) {
-                    _githubUserDetail.value = response.body()
-                } else {
-                    Log.e(ContentValues.TAG, "onFailure: ${response.message()}")
+        viewModelScope.launch(Dispatchers.IO) {
+            val client = ApiConfig.getApiService().getGithubUserDetail(username)
+            client.enqueue(object : Callback<DetailUserResponse> {
+                override fun onResponse(
+                    call: Call<DetailUserResponse>,
+                    response: Response<DetailUserResponse>
+                ) {
+                    _isLoading.value = false
+                    if (response.isSuccessful) {
+                        _githubUserDetail.value = response.body()
+                    } else {
+                        Log.e(ContentValues.TAG, "onFailure: ${response.message()}")
+                    }
                 }
-            }
 
-            override fun onFailure(call: Call<DetailUserResponse>, t: Throwable) {
-                _isLoading.value = false
-                Log.e(ContentValues.TAG, "onFailure: ${t.message.toString()}")
-            }
-        })
+                override fun onFailure(call: Call<DetailUserResponse>, t: Throwable) {
+                    _isLoading.value = false
+                    Log.e(ContentValues.TAG, "onFailure: ${t.message.toString()}")
+                }
+            })
+        }
     }
 }
